e069ca1e0883f66937c4186a701c4941e22f74d4
mmovieworld <88450594+mmovieworld@users.noreply.github.com>
2021-10-22 18:49:10 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -76,10 +76,10 @@ def get_reply_markup(username, query):
         InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
         InlineKeyboardButton('Share bot', url=url),
     ],]
-        InlineKeyboardButton('👥 Official Group 👥', url='https://t.me/mmovie_world_Group')
-        InlineKeyboardButton('🔗 MMovie World Links 🔗', url="https://t.me/mmovieworldlinks")
+        InlineKeyboardButton('👥Official Group👥', url='https://t.me/mmovie_world_Group')
+        InlineKeyboardButton('🔗MMovie World Links🔗', url="https://t.me/mmovieworldlinks")
     ],]
-        InlineKeyboardButton('➕ Request Movie ➕', url="https://t.me/Contact_MMovie_World_Bot")
+        InlineKeyboardButton('➕Request Movie➕', url="https://t.me/Contact_MMovie_World_Bot")
     ]]
     return InlineKeyboardMarkup(buttons)
 
